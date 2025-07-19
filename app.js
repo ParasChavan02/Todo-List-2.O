8102aa6a02ac0cc03e942838fe38e6f06ef71845
ParasChavan02 <paraschavan0201@gmail.com>
2025-07-19 17:57:21 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+var app=express();
+app.set("view engine","ejs");
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('Public'));
+
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/todo");
+const trySchema=new mongoose.Schema({
+    name:String
+});
+const item =mongoose.model("task",trySchema);
+const todo=new item ({
+    name:"Create some videos"
+});
+const todo2=new item ({
+    name:"Learn DSA"
+});
+const todo3=new item ({
+    name:"Learn React"
+});
+const todo4=new item ({
+    name:"Take some rest"
+}); 
+// todo2.save();
+// todo3.save();
+// todo4.save();
+
+app.get("/", async function(req, res) {
+  try {
+    const foundItems = await item.find({});
+    res.render("index", { dayej: foundItems });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/",function(req,res){
+    const itemName=req.body.ele1;
+    const todo4=new item({
+      name:itemName
+    });
+    todo4.save();
+    res.redirect("/");
+});
+
+app.post("/delete", async function(req, res) {
+    const checked = req.body.checkbox1;
+    try {
+        await item.findByIdAndDelete(checked);
+        console.log("deleted");
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.listen("8000",function(){
+     console.log("Server is runnning");
+});
